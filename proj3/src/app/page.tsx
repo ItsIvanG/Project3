91df64d7c637e03f64014fc1e678150b04dfc5e0
ItsIvanG <itsivangbiz@gmail.com>
2025-02-25 18:44:27 +0800

    fix conflict danielle

--- proj3/src/app/page.tsx
+++ proj3/src/app/page.tsx
@@ -1,18 +1,18 @@
-import { VisitorPricing } from '@/components/VisitorPricing';
-import { VisitorsAbout } from '@/components/VisitorsAbout';
-import { VisitorsAnalytics } from '@/components/VisitorsAnalytics';
-import { VisitorsCourses } from '@/components/VisitorsCourses';
-import { VisitorsHome } from '@/components/VisitorsHome';
-import { VisitorsNavbar } from '@/components/Navbar';
-import { VisitorsTestimonials } from '@/components/VisitorsTestimonials';
-import { VisitorsQualities } from '@/components/VisitorsQualities';
-import { VisitorsMotto } from '@/components/VisitorsMotto';
-import { Footer } from '@/components/Footer';
+import { VisitorPricing } from "@/components/VisitorPricing";
+import { VisitorsAbout } from "@/components/VisitorsAbout";
+import { VisitorsAnalytics } from "@/components/VisitorsAnalytics";
+import { VisitorsCourses } from "@/components/VisitorsCourses";
+import { VisitorsHome } from "@/components/VisitorsHome";
+import { Navbar } from "@/components/Navbar";
+import { VisitorsTestimonials } from "@/components/VisitorsTestimonials";
+import { VisitorsQualities } from "@/components/VisitorsQualities";
+import { VisitorsMotto } from "@/components/VisitorsMotto";
+import { Footer } from "@/components/Footer";
 
 export default function Page() {
   return (
     <>
-      <VisitorsNavbar />
+      <Navbar />
       <VisitorsHome />
       <VisitorsAnalytics />
       <VisitorsCourses />
